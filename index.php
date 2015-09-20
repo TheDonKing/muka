29f1e29055435238de169fec33fb58003eb97a8f
TheDonKing <starspersona@gmail.com>
2015-09-20 16:06:21 +0300

    Начало проекта

---
+++ index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html lang="ru" class="lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html lang="ru" class="lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html lang="ru" class="lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="ru">
+<!--<![endif]-->
+<head>
+    <!--Created by DonKing
+    Base template (without user's data) checked by http://s-dt.ru-->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="author" lang="ru" content="s-dt.ru"/>
+    <meta name="copyright" content="&copy; DTStudio <?php echo date("Y"); ?> "/>
+    <meta name="keywords" content="FraemWork"/>
+    <meta name="robots" content="all"/>
+    <meta name="description" content="Framework">
+    <title>Framework</title>
+    <meta name='yandex-verification' content='6c33212f4dc14e06' />
+
+    <!-- Для нормального отображении сайта в мобильных устройствах  -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="HandheldFriendly" content="true">
+
+    <!-- SSS -->
+    <link rel="DC.identifier" type="text/plain" href="http://#">
+    <link rel="sitemap" type="application/xml" title="Sitemap" href="sitemap.xml" />
+    <link rel="meta" type="application/rdf+xml" title="Dublin" href="dublin.rdf" />
+    <link rel="search" type="application/opensearchdescription+xml" title="DTstudio" href="opensearch.xml" />
+    <meta name="robots" content="index, follow">
+    <meta name='yandex-verification' content='4c5619304dc1f8d7' />
+
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <!-- For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: -->
+    <link rel="apple-touch-icon-precomposed" href="apple-touch-icon.png">
+    <!-- For the iPad mini and the first- and second-generation iPad on iOS ≤ 6: -->
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="apple-touch-icon-72x72.png">
+    <!-- For the iPad mini and the first- and second-generation iPad on iOS ≤ 7: -->
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="apple-touch-icon-76x76.png">
+    <!-- For iPhone with high-resolution Retina display running iOS ≤ 6: -->
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="apple-touch-icon-114x114.png">
+    <!-- For iPhone with high-resolution Retina display running iOS ≤ 7: -->
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="apple-touch-icon-120x120.png">
+    <!-- For iPad with high-resolution Retina display running iOS ≤ 6: -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="apple-touch-icon-144x144.png">
+    <!-- For iPad with high-resolution Retina display running iOS ≤ 7: -->
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="apple-touch-icon-152x152.png">
+
+    <!-- Libs -->
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap.min.css" type="text/css" />
+	<link rel="stylesheet" href="libs/font-awesome-4.2.0/css/font-awesome.min.css" type="text/css" />
+	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" type="text/css" />
+	<link rel="stylesheet" href="libs/owl-carousel/owl.carousel.css" type="text/css" />
+	<link rel="stylesheet" href="libs/countdown/jquery.countdown.css" type="text/css" />
+
+    <!-- Root Style -->
+	<link rel="stylesheet" href="css/main.css" type="text/css" />
+    <link rel="stylesheet" href="css/fonts.css" type="text/css"/>
+    <link rel="stylesheet" href="css/ie.css" type="text/css" />
+	<link rel="stylesheet" href="css/media.css" type="text/css"/>
+
+    <script type="text/javascript" src="js/modernizr.custom.js"></script>
+
+    <!-- Пишем стили для каждой версии IE -->
+    <!--[if IEMobile 7 ]> <html lang="ru" class=" iem7"> <![endif]-->
+    <!--[if lt IE 7 ]> <html lang="ru" class="ie6 oldie"> <![endif]-->
+    <!--[if IE 7 ]> <html lang="ru" class="ie7 oldie"> <![endif]-->
+    <!--[if IE 8 ]> <html lang="ru" class="ie8 oldie"> <![endif]-->
+    <!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html lang="ru"><!--<![endif]-->
+
+    <!--if lte IE 8]>
+    <script src="libs/respond/respond.min.js"/></script>
+    <![endif]-->
+
+</head>
+<body>
+<!--[if lt IE 8]>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+<header>
+    <h1><a href="#">Стандартный блог</a></h1>
+</header>
+<nav>
+    <ul>
+        <li><a href="#">Главная</a></li>
+        <li><a href="#">Архив</a></li>
+        <li><a href="#">О блоге</a></li>
+        <li><a href="#">Контакты</a></li>
+    </ul>
+</nav>
+<section>
+    <article>
+        <header>
+            <h1><a href="#">Заголовок</a></h1>
+        </header>
+        <section>
+            <p>Тут текст...</p>
+        </section>
+    </article>
+    <article>
+        <header>
+            <h1><a href="#">Заголовок</a></h1>
+        </header>
+        <section>
+            <p>Тут текст...</p>
+        </section>
+    </article>
+    <article>
+        <header>
+            <h1><a href="#">Заголовок</a></h1>
+        </header>
+        <section>
+            <p>Тут текст...</p>
+        </section>
+    </article>
+</section>
+<footer>
+    <p>Copyright © 2008 All Rights<p>
+</footer>
+
+<!-- @TODO Добавить комент -->
+
+<!-- Тут подключаем скрипты -->
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script type="text/javascript" src="libs/respond/respond.min.js"></script>
+    <script type="text/javascript" src="libs/jquery-mousewheel/jquery.mousewheel.min.js"></script>
+	<script type="text/javascript" src="libs/fancybox/jquery.fancybox.pack.js"></script>
+	<script type="text/javascript" src="libs/waypoints/waypoints-1.6.2.min.js"></script>
+	<script type="text/javascript" src="libs/scrollto/jquery.scrollTo.min.js"></script>
+	<script type="text/javascript" src="libs/owl-carousel/owl.carousel.min.js"></script>
+	<script type="text/javascript" src="libs/countdown/jquery.plugin.js"></script>
+	<script type="text/javascript" src="libs/countdown/jquery.countdown.min.js"></script>
+	<script type="text/javascript" src="libs/countdown/jquery.countdown-ru.js"></script>
+	<script type="text/javascript" src="libs/landing-nav/navigation.js"></script>
+	<script type="text/javascript" src="js/common.js"></script>
+	<!-- Yandex.Metrika counter --><!-- /Yandex.Metrika counter -->
+	<!-- Google Analytics counter --><!-- /Google Analytics counter -->
+</body>
+</html>
